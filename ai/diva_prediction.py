93897b02a55377a1429889dc1c83a3661545ce7c
wamika17 <varadawamika@gmail.com>
2026-07-16 23:52:46 +0530

    Update diva_prediction.py

--- ai/diva_prediction.py
+++ ai/diva_prediction.py
@@ -1,1 +1,26 @@
 
+def predict_diva_score(
+
+    bmi,
+
+    vein_visibility,
+
+    skin_type
+
+):
+
+    score = 0
+
+    if bmi > 30:
+
+        score += 2
+
+    if vein_visibility == "Poor":
+
+        score += 2
+
+    if skin_type == "Dark":
+
+        score += 1
+
+    return score
