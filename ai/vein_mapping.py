714ecd09a1b065576358091493820d9797c2c736
wamika17 <varadawamika@gmail.com>
2026-07-16 23:51:31 +0530

    Add vein detection function in vein_mapping.py

--- ai/vein_mapping.py
+++ ai/vein_mapping.py
@@ -1,1 +1,24 @@
+"""
+--------------------------------------------------------
+Vein Mapping
 
+Author: Wamika Varada
+--------------------------------------------------------
+"""
+
+import cv2
+
+
+def detect_veins(segmented_image):
+
+    contours, _ = cv2.findContours(
+
+        segmented_image,
+
+        cv2.RETR_EXTERNAL,
+
+        cv2.CHAIN_APPROX_SIMPLE
+
+    )
+
+    return contours
