578e7c4d34bbc84547e944497ae0eab0a0aed4fe
wamika17 <varadawamika@gmail.com>
2026-07-16 23:57:13 +0530

    Add mock patient data for dashboard

---
+++ dashboard/dashboard_mock_data.py
@@ -0,0 +1,19 @@
+patient = {
+
+    "Name":"John Doe",
+
+    "Age":27,
+
+    "Heart Rate":72,
+
+    "SpO2":98,
+
+    "BMI":24.5,
+
+    "Vein Visibility":"Good",
+
+    "DIVA Score":2,
+
+    "Recommendation":"Left Median Cubital Vein"
+
+}
