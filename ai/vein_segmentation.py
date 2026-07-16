9ce7ffb8052a91d9b0cef92f49ba1940352bcce2
wamika17 <varadawamika@gmail.com>
2026-07-16 23:51:20 +0530

    Add vein segmentation function
    Implemented vein segmentation function using adaptive thresholding.

--- ai/vein_segmentation.py
+++ ai/vein_segmentation.py
@@ -1,1 +1,34 @@
+"""
+--------------------------------------------------------
+Vein Segmentation
 
+Author: Wamika Varada
+--------------------------------------------------------
+"""
+
+import cv2
+
+
+def segment_veins(image):
+
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    blurred = cv2.GaussianBlur(gray, (5,5), 0)
+
+    segmented = cv2.adaptiveThreshold(
+
+        blurred,
+
+        255,
+
+        cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+
+        cv2.THRESH_BINARY,
+
+        11,
+
+        2
+
+    )
+
+    return segmented
